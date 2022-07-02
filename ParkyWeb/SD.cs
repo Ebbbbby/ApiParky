90122720d4417da8631fa61e39c8d7517b891b27
ebbbbby <ocynthiaebele@gmail.com>
2022-07-02 16:50:22 +0100

    Working on the Park Web

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44339/";
+        public static string NationalParkApiPath = APIBaseUrl+ "api/v1/nationalparks";
+        public static string TrailApiPath= APIBaseUrl+ "api/v1/trails";
+
+    }
+}
